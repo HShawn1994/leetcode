51b3a10d17d8d7b1f9ec4a7e214f3f56ec81e1d9
shang.huang <shang.huang@zstack.io>
2018-12-26 10:27:03 +0800

    update 942 answer

---
+++ math/942_Di_String_Match.js
@@ -0,0 +1,37 @@
+// 题目：增减字符串匹配
+
+/**
+给定只含 "I"（增大）或 "D"（减小）的字符串 S ，令 N = S.length。
+返回 [0, 1, ..., N] 的任意排列 A 使得对于所有 i = 0, ..., N-1，都有：
+如果 S[i] == "I"，那么 A[i] < A[i+1]
+如果 S[i] == "D"，那么 A[i] > A[i+1]
+
+示例 1：
+
+输出："IDID"
+输出：[0,4,1,3,2]
+示例 2：
+
+输出："III"
+输出：[0,1,2,3]
+示例 3：
+
+输出："DDI"
+输出：[3,2,0,1]
+**/
+
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var diStringMatch = function(S) {
+  let numd = S.length
+  let numi = 0
+  let res = []
+  for (let i = 0; i < S.length; i++) {
+      if (S[i] === 'I') res.push(numi++)
+      else res.push(numd--)
+  }
+  res.push(numd)
+  return res
+};
