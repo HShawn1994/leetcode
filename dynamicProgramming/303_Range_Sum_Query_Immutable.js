505ffab684f46c3a25e16dd7b35ade92ec19f333
shang.huang <shang.huang@zstack.io>
2019-01-08 23:35:32 +0800

    [update] update answers of dynamic programing

---
+++ dynamicProgramming/303_Range_Sum_Query_Immutable.js
@@ -0,0 +1,41 @@
+/*
+区域和检索-数组不可变
+给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+
+示例：
+
+给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+说明:
+
+你可以假设数组不可变。
+会多次调用 sumRange 方法。
+*/
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.res = [nums[0]]
+    for (let i = 1; i < nums.length; i++) {
+        this.res.push(nums[i] + this.res[i - 1])
+    }
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    return i === 0 ? this.res[j] : this.res[j] - this.res[i - 1]
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
